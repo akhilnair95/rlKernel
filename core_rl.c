5ec16f1f52cbe96c52a7dc0f28150fc242f54697
akhilnair95 <akhilnair95@gmail.com>
2019-06-13 22:13:31 +0530

    Q lr; seq upd; not workin

--- core_rl.c
+++ core_rl.c
@@ -17,8 +17,8 @@ unsigned long flags;
 
 //Function prototype declaration
 int calculate_Qstate(int cpu, struct task_struct *p);
-int calculate_state(void);
-void update_weights(void);
+void update_weights(struct task_struct *p);
+int best_action(struct task_struct *p, int next_state[]);
 
 int max_index(int Qval[]);
 long calculate_reward(void);
@@ -42,53 +42,69 @@ int timer_tick = 0;
 
 int select_task_rq_rl(struct task_struct *p, int cpu_given, int sd_flags, int wake_flags){
 	
+	int cpu_chosen;
 	int next_state[NR_CPU];
-	int cpu, cpu_chosen;
-
-	int is_acquire = 0;
-	int state;
 
 	unsigned int coin;
 
-	// Find a better way to do this
-	spin_lock_irqsave(&mLock,flags);
-	if(cpu_lock == -1){
-		trace_printk("Lock Acquired\n");
-		cpu_lock = -2;
-		is_acquire = 1;
-	}
-	spin_unlock_irqrestore(&mLock,flags);
+	update_weights(p);
+
+	cpu_chosen = best_action(p,next_state);
 	
+	// With prob of epsilon, make random action
+	coin = get_random_int() % precision;
+	if(coin <= epsilon){
+		trace_printk("Random Action\n");
+		cpu_chosen =  get_random_int() % NR_CPU;
+	}else{
+		trace_printk("Deterministic Action\n");
+	}
+
+	// Store prevState [ To be used in next weight update ] 
+	prev_state = next_state[cpu_chosen];
+			
+	trace_printk("CPU chosen: %d\n",cpu_chosen);	
+		
+	return cpu_chosen;
+}
+
+// Weight update functions
+
+void update_weights(struct task_struct *p){
+	long R, delta, Vval;
+	int action, next_state[NR_CPU], state;
+
+	R = calculate_reward();	
+
+	// Max Q(s',a')
+	action = best_action(p, next_state);
+	Vval = QVals[next_state[action]];
+	
+	delta = (R + mul(gamma, Vval)) - QVals[prev_state];
+
+	printfp("Reward", R);
+	printfp("Vval", Vval);
+	printfp("prev_Vval", QVals[prev_state]);	
+	printfp("Delta", delta);
+
+	QVals[prev_state] += mul(alpha, delta);
+
+	for(state = 0; state < NR_STATE;state++){
+		printfp("Qval", QVals[state]);
+	}	
+}
+
+int best_action(struct task_struct *p, int next_state[]){
+	
+	int cpu;
+
 	for(cpu=0;cpu<NR_CPU;cpu++){
 		// Get the current state f(s,a)		
-		state = calculate_Qstate(cpu,p);
-		// Get Q(s,a) 
-		next_state[cpu] = state;		
+		next_state[cpu] = calculate_Qstate(cpu,p);		
 	}
 
 	// Max over all Q(s,a)
-	cpu_chosen = max_index(next_state);
-	
-	if(is_acquire == 1){
-
-		// With prob of epsilon, make random action
-		coin = get_random_int() % precision;
-		if(coin <= epsilon){
-			trace_printk("Random Action\n");
-			cpu_chosen =  get_random_int() % NR_CPU;
-		}else{
-			trace_printk("Deterministic Action\n");
-		}
-
-		// Store prevState [ To be used in next weight update ] 
-		prev_state = next_state[cpu_chosen];
-				
-		cpu_lock = cpu_chosen;
-
-		trace_printk("CPU chosen: %d\n",cpu_chosen);	
-	}
-		
-	return cpu_chosen;
+	return max_index(next_state);
 }
 
 void get_nr_process(long proc[]){
@@ -141,28 +157,6 @@ int calculate_Qstate(int cpu, struct task_struct *p){
 		trace_printk("\n");		
 	}
 	
-	return 2*is_hit + is_ghost;	
-}
-
-int calculate_state(){
-	
-	long proc[NR_CPU];
-	int is_hit, is_ghost;
-	int cpu;
-
-	is_hit = (prev_state == 2) || (prev_state == 3);
-
-	get_nr_process(proc);
-
-	for(cpu=0; cpu<NR_CPU; cpu++){
-		printfp("Nr_pr",proc[cpu]*precision);
-	}
-
-	is_ghost = get_proc_variance(proc);
-
-	printfp("is_hit", is_hit);
-	printfp("is_ghost", is_ghost);
-
 	return 2*is_hit + is_ghost;	
 }
 
@@ -220,34 +214,9 @@ int max_index(int state[]){
 	return index;
 }
 
-// Weight update functions
-
-void update_weights(){
-	long R, delta, Vval;
-	int state;
-
-	R = calculate_reward();	
-	
-	state = calculate_state();
-
-	Vval = QVals[state];
-	
-	delta = (R + mul(gamma, Vval)) - QVals[prev_state];
-
-	printfp("Reward", R);
-	printfp("Vval", Vval);
-	printfp("prev_Vval", QVals[prev_state]);	
-	printfp("Delta", delta);
-
-	QVals[prev_state] += mul(alpha, delta);
-
-	for(state = 0; state < NR_STATE;state++){
-		printfp("Qval", QVals[state]);
-	}	
-}
 
 void scheduler_tick_rl(int cpu){
-	if(cpu != cpu_lock)
+	/*if(cpu != cpu_lock)
 		return;
 
 	timer_tick++;
@@ -256,7 +225,7 @@ void scheduler_tick_rl(int cpu){
 		update_weights();
 		cpu_lock = -1;
 		timer_tick = 0;
-	}
+	}*/
 }
 
 // Fixed point simulation functions
